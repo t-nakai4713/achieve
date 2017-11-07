95157b983842bc58a7e034786039a325d520c26e
t nakai <t-nakai@nttpc.co.jp>
2017-11-07 04:35:33 +0000

    UserImgUpload

--- Gemfile
+++ Gemfile
@@ -66,6 +66,10 @@ gem 'omniauth'
 gem 'omniauth-twitter'
 gem 'omniauth-facebook'
 
+#image uploader
+gem 'carrierwave'
+gem 'mini_magick'
+
 group :development do
   gem 'letter_opener_web'
   gem 'dotenv-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   has_many :blogs
+  mount_uploader :avatar, AvatarUploader
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(provider: auth.provider, uid: auth.uid)
@@ -44,5 +45,16 @@ class User < ActiveRecord::Base
   def self.create_unique_string
     SecureRandom.uuid
   end
+
+  def update_with_password(params, *options)
+    if provider.blank?
+      super
+    else
+      params.delete :current_password
+      update_without_password(params, *options)
+    end
+  end
+
+
 
 end
