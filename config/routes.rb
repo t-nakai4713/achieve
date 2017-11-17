b96a77cbf56bbd77fb5ba0533d1c3efc1b558412
t nakai <t-nakai@nttpc.co.jp>
2017-11-17 07:24:21 +0000

    message

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,12 @@ Rails.application.routes.draw do
 
   resources :poems, only: [:index, :show]
 
+
+resources :conversations do
+  resources :messages
+end
+
+
   root 'top#index'
 
 if Rails.env.development?
